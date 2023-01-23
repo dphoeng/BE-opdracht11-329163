a36c0e75513b8543d43c17689c9a6e920e56f732
dphoeng <329163@student.mboutrecht.nl>
2023-01-23 10:25:20 +0100

    added voertuigen page

--- app/controllers/Instructeurs.php
+++ app/controllers/Instructeurs.php
@@ -39,4 +39,45 @@ class Instructeurs extends Controller
 		];
 		$this->view("instructeurs/index", $data);
 	}
+
+	public function voertuigen($id = null)
+	{
+		$rows = '';
+		$sterren = "";
+		$naam = "";
+		$datumInDienst = "";
+
+		$instructeur = $this->instructeurModel->getInstructeurById($id);
+
+		if ($instructeur) {
+			for ($i = 0; $i < $instructeur->AantalSterren; $i++) {
+				$sterren .= "★";
+			}
+			$naam = $instructeur->Tussenvoegsel ? `$instructeur->Voornaam $instructeur->Tussenvoegsel $instructeur->Achternaam` : `$instructeur->Voornaam $instructeur->Achternaam`;
+			$datumInDienst = $instructeur->DatumInDienst;
+
+			// haalt de gegevens uit de database via the model
+			$record = $this->instructeurModel->getVoertuigenByInstructeurId();
+			if ($record) {
+				foreach ($record as $value) {
+
+					$rows .= "<tr>
+										<td>$value->Voornaam</td>
+										<td>$value->Tussenvoegsel</td>
+										<td>$value->Achternaam</td>
+										<td>$value->Mobiel</td>
+										<td>$value->DatumInDienst</td>
+										<td>$sterren</td>
+										<td><a href='" . URLROOT . "/instructeurs/voertuigen/$value->Id'><img src='" . URLROOT . "/img/book.png' alt='book'></a></td>
+								</tr>";
+				}
+			}
+		}
+
+		// data die wordt doorgestuurd naar de view
+		$data = [
+			"rows" => $rows, "aantal" => count($record), "naam" => $naam, "sterren" => $sterren, "datumInDienst" => $datumInDienst
+		];
+		$this->view("instructeurs/voertuigen", $data);
+	}
 }
--- app/models/Instructeur.php
+++ app/models/Instructeur.php
@@ -12,7 +12,21 @@ class Instructeur
 
 	public function getInstructeurs()
 	{
-		$this->db->query("SELECT * FROM `Instructeur`");
+		$this->db->query("SELECT * FROM `Instructeur` ORDER BY `AantalSterren` DESC");
 		return $this->db->resultSet();
+	}
+
+	public function getVoertuigenByInstructeurId($id)
+	{
+		$this->db->query("SELECT * FROM `Voertuig` voe INNER JOIN `VoertuigInstructeur` vin ON vin.VoertuigId = voe.Id WHERE vin.InstructeurId = :id");
+		$this->db->bind(":id", $id, PDO::PARAM_INT);
+		return $this->db->resultSet();
+	}
+
+	public function getInstructeurById($id)
+	{
+		$this->db->query("SELECT * FROM `Instructeur` WHERE `Id` = :id");
+		$this->db->bind(":id", $id, PDO::PARAM_INT);
+		return $this->db->single();
 	}
 }
